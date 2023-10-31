d011b2f9359059c7a6f303105297b30053dd69e5
suraj328 <shahsuraj328@gmail.com>
2023-10-31 10:13:02 +0545

    added satck posined view

--- lib/screen/meal_screen.dart
+++ lib/screen/meal_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meal_app/models/meal.dart';
+import 'package:meal_app/widget/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   final String title;
@@ -26,7 +27,7 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
           itemCount: meals.length,
-          itemBuilder: (ctx, index) => Text(meals[index].title));
+          itemBuilder: (ctx, index) => MealItem(meal: meals[index]));
     }
     return Scaffold(
       appBar: AppBar(
---
+++ lib/widget/meal_item.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/models/meal.dart';
+import 'package:meal_app/widget/meal_item_trait.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class MealItem extends StatelessWidget {
+  const MealItem({super.key, required this.meal});
+
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
+  }
+
+  String get affordabilityText {
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);
+  }
+
+  final Meal meal;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      // Enforce child element to use properties as parent have
+      // Stack take full space here, clip beahvior apply forcefully to apply parent properties like margin
+      clipBehavior: Clip.hardEdge,
+      // give a shadow
+      elevation: 2,
+      child: InkWell(
+        onTap: () {},
+        // the Stack widget is used to overlay multiple children widgets on top of each other.
+        // It allows you to create complex layouts by positioning widgets at specific coordinates within the stack.
+        // Each child in a Stack can be precisely positioned using the Positioned widget.
+        // Here's an example of how to use the Stack widget in Flutter:
+        child: Stack(
+          children: [
+            FadeInImage(
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+              fit: BoxFit.cover,
+              height: 200,
+              width: double.infinity,
+            ),
+            // stack come with positioned, helps to make alignment for element
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                color: Colors.black54,
+                padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 4),
+                child: Column(
+                  children: [
+                    Text(
+                      meal.title,
+                      maxLines: 2,
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MealItemTrait(
+                          icon: Icons.schedule,
+                          label: '${meal.duration} min',
+                        ),
+                        const SizedBox(
+                          width: 12,
+                        ),
+                        MealItemTrait(
+                          icon: Icons.work,
+                          label: complexityText,
+                        ),
+                        const SizedBox(
+                          width: 12,
+                        ),
+                        MealItemTrait(
+                          icon: Icons.attach_money,
+                          label: affordabilityText,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
